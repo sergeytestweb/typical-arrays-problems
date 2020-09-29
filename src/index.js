fed42665fc2dd15749022a263f1386601277ee91
Sergey Pozharitsky <pozhar511@gmail.com>
2020-09-29 20:41:48 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    if (array && array.length) {
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length) {
+        return Math.max.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length) {
+        let avr = 0;
+        for (let elem of array) {
+            avr += elem / array.length;
+        }
+        return avr.toFixed(2);
+    } else {
+        return 0;
+    }
+};
